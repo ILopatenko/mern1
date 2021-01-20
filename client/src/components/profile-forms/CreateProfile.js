bcd9d628c3c833a7892db947df46c129f5295f7e
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-19 19:55:19 -0800

    Finish CREATE and UPDATE general information on user profile. L9.4

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
 
     const [formData, setFormData] = useState({
         company: '',
@@ -19,7 +21,7 @@ const CreateProfile = props => {
         instagram: ''
     });
 
-    const [displaySosialInputs, toggleSosialInputs] = useState(false);
+    const [displaySosialInputs, toggleSocialInputs] = useState(false);
 
     const {
         company,
@@ -38,6 +40,11 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history);
+    };
+
     return (
         <Fragment>
             <h1 className="large text-primary">
@@ -48,7 +55,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
             <small>* = required field</small>
-            <form className="form">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                     <select name="status" value={status} onChange={e => onChange(e)}>
                         <option value="0">* Select Professional Status</option>
@@ -107,7 +114,7 @@ const CreateProfile = props => {
                 </div>
 
                 <div className="my-2">
-                    <button onClick={() => toggleSosialInputs(!displaySosialInputs)} type="button" className="btn btn-light">
+                    <button onClick={() => toggleSocialInputs(!displaySosialInputs)} type="button" className="btn btn-light">
                         Add Social Network Links
           </button>
                     <span>Optional</span>
@@ -149,7 +156,8 @@ const CreateProfile = props => {
 };
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired,
 };
 
-export default CreateProfile;
+
+export default connect(null, { createProfile })(withRouter(CreateProfile));
